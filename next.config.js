884fc3b5e12f0f13a52481e0b1f0640d5f8094c1
Bao Nguyen <vikamicanada@gmail.com>
2026-01-04 22:45:33 -0700

    Remove /api/posts for static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ["https://cho.vikami.ca"],
+  output: "export",
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
